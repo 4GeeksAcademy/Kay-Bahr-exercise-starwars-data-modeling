c46342366c54a7b125e769ae24e4ba10d69b80b3
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-05-06 01:05:44 +0000

    base structure commit

--- src/models.py
+++ src/models.py
@@ -10,36 +10,60 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'User'
 
-    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    name_last = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
     def serialize(user):
         return {
-            "id": user.id,
-            "name": user.name
+            "id": user.user_id,
+            "name": user.name,
+            "last name": user.name_last,
+            "email": user.email
+        }
+    
+class Login(Base):
+    __tablename__ = 'Login'
+
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    username = Column(String(250), primary_key=True)
+    password = Column(String(250), nullable=False)
+
+    def serialize(login):
+        return {
+            "id": login.user_id,
+            "username": login.username,
+            "password": login.password
         }
 
+class Favorites(Base):
+    __tablename__ = 'Favorites'
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    list_id = Column(Integer, primary_key=True)
+    item_name = Column(String(250), ForeignKey('items.item_name'))
+    user_id = Column(Integer, ForeignKey('user.user_id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    def serialize(favorites):
+        return {
+            "list id": favorites.list_id,
+            "item name": favorites.item_name,
+            "user id": favorites.user_id
+        }
 
-    def to_dict(self):
-        return {}
+class Items(Base):
+    __tablename__ = 'Items'
+
+    item_name = Column(String(250), primary_key=True)
+    item_type = Column(String(250), nullable=False)
+    data = Column(String(250), nullable=False)
+
+    def serialize(items):
+        return {
+            "name": items.item_name,
+            "type": items.item_type,
+            "data": items.data
+        }
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
